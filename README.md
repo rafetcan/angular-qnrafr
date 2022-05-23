7636b38a5e47a90df533ee096c99c9dd9c032857
rafetcan <rafethokka@gmail.com>
2022-05-23 13:28:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-qnrafr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-qnrafr)
---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-world',
+  template: `
+    <h2>Merhaba Dünya</h2>
+    <p>Burası ilk </p>
+  `, //
+  // templateUrl diyerek url yerini tanımlıyoruz.
+})
+export class HelloWorldComponent {
+  // The code in this class drives the component's behavior.
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
